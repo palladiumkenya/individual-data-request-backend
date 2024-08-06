09af9152693c62f79c0d2ec8b7ac6be5ae5d4157
NewtonMutugi <newtonmut197@gmail.com>
2024-08-06 14:43:01 +0300

    Added godotenv for .env usage

--- server/Server.go
+++ server/Server.go
@@ -1,7 +1,12 @@
 package server
 
 import (
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"github.com/palladiumkenya/individual-data-request-backend/routes"
 )
 
@@ -9,6 +14,16 @@ var router = gin.Default()
 
 func Run() {
 	routes.Handlers(router)
+
+	// Load .env file
+	godotenv.Load(".env")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %s", err)
+	}
+	mailChimp := os.Getenv("MAILGUN_KEY")
+	fmt.Printf("Mail Chimp: %s", mailChimp)
+
 	// Start server on port 8080
 	router.Run(":8000")
 }
