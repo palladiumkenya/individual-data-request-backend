8a87c654207032818caa715ac80636aaf64aed17
NewtonMutugi <newtonmut197@gmail.com>
2024-08-06 12:52:18 +0300

    Added test controller

--- controllers/ApiHealth.go
+++ controllers/ApiHealth.go
@@ -2,6 +2,9 @@ package controllers
 
 import "github.com/gin-gonic/gin"
 
-func GetApiHealth(c *gin.Context){
-		c.JSON(200, "Server is up and running!")
+func GetApiHealth(c *gin.Context) {
+	/*
+		This is a test controller
+	*/
+	c.JSON(200, "Server is up and running!")
 }
