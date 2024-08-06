d183840cbdf077b7aeb69decdb530d003c7ca331
NewtonMutugi <newtonmut197@gmail.com>
2024-08-06 12:48:02 +0300

    Revert "Created server config file"
    This reverts commit 2430e867d0b51ab5b04d14b1230fda8c88a08d6e.

--- controllers/ApiHealth.go
+++
@@ -1,7 +0,0 @@
-package controllers
-
-import "github.com/gin-gonic/gin"
-
-func GetApiHealth(c *gin.Context){
-		c.JSON(200, "Server is up and running!")
-}
--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "github.com/palladiumkenya/individual-data-request-backend/server"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	server.Run()
+	// Create new Gin router
+	router := gin.Default()
+
+	// Test endpoint
+	router.GET("/api_health", func(ctx *gin.Context) {
+		ctx.String(200, "Server is up and running!")
+	})
+
+	// Start server on port 8080
+	router.Run(":8080")
 }
