180da89d7a640a9a121530c3d5cd29a5f8f5f006
NewtonMutugi <newtonmut197@gmail.com>
2024-08-13 16:53:02 +0300

    feat: Update Review model with UUID primary keys and foreign keys

--- internal/models/Review.go
+++ internal/models/Review.go
@@ -2,23 +2,25 @@ package models
 
 import (
 	"time"
+
+	"github.com/gofrs/uuid"
 )
 
 type Review struct {
-	ID             uint   `gorm:"primaryKey"`
-	Content        string `gorm:"type:text;not null"`
-	UserID         uint   `gorm:"not null"`
-	ReviewerID     uint   `gorm:"not null"`
-	ReviewThreadID uint   `gorm:"not null"`
+	ID             uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
+	Content        string    `gorm:"type:text;not null"`
+	UserID         uint      `gorm:"not null"`
+	ReviewerID     uuid.UUID `gorm:"type:uuid"`
+	ReviewThreadID uuid.UUID `gorm:"type:uuid"`
 	CreatedAt      time.Time
 	UpdatedAt      time.Time
 }
 
 type ReviewThread struct {
-	ID        uint   `gorm:"primaryKey"`
-	Title     string `gorm:"not null"`
-	UserID    uint   `gorm:"not null"`
-	RequestID uint   `gorm:"not null"`
+	ID        uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
+	Title     string    `gorm:"not null"`
+	UserID    uint      `gorm:"not null"`
+	RequestID uuid.UUID `gorm:"type:uuid"`
 	CreatedAt time.Time
 	Reviews   []Review `gorm:"foreignKey:ReviewThreadID"`
 }
