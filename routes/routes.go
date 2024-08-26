530d9edb14d0008dd66f65e715d856bc4d25474c
marykilewe <marykilewe@gmail.com>
2024-08-26 10:44:37 +0300

    update paths in routes

--- routes/routes.go
+++ routes/routes.go
@@ -10,13 +10,14 @@ func Handlers(router *gin.Engine) {
 	router.GET("/api_health", controllers.GetApiHealth) // Test endpoint
 	router.POST("/send_mail", controllers.SendMail)     // Send test email
 
-	router.GET("/requests", controllers.GetRequests)                      // get requests
+	router.GET("/requests", controllers.GetRequests) // get requests
 
 	router.POST("/upload", controllers.UploadFile)
 
-	router.GET("/internal_approval/:id", controllers.GetInternalApproval) // get approval page data
-	router.POST("/internal_approval/action", controllers.ApproverAction)  // approve or reject requests
-	router.GET("/approval/:type/:id", controllers.GetApproval)            // get approval page data
+	router.GET("/approvals/:type", controllers.GetAllApprovals)          // get all approvals
+	router.POST("/internal_approval/action", controllers.ApproverAction) // approve or reject requests
+	router.GET("/approval/:type/:id", controllers.GetApproval)           // get approval page data
+	router.GET("/approvals/count/:type", controllers.GetApprovalsCount)  // get all approvals
 
 	router.POST("/new_review_thread", controllers.CreateReviewThread)      // create review thread
 	router.POST("/add_review", controllers.AddReview)                      // add review
